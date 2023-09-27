fb3880aed3ed1e470833f4be0469a0cdf267a8a5
Zoya Nasir <k22031112@kcl.ac.uk>
2023-09-27 13:53:33 +0100

    Completed Excercise

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        key = str(item)
+
+        if key in frequencies:
+            frequencies [key] += 1
+
+        else:
+                frequencies[key] = 1
+    
     return frequencies
